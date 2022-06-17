ba7ef97ea658ec3a3493675077255ccd8fa04545
VovikZip <alioshkin75@gmail.com>
2022-06-17 17:49:32 +0300

    Complete Lab5

--- index.js
+++ index.js
@@ -1,21 +1,46 @@
-const { generatePrimeNumber } = require('./src/bignum/bignum');
-const { log, logError } = require('./src/logger');
-const { operations } = require('./src/operations/operations');
+'use strict';
+
+const crypto = require('crypto');
+
+const generateKeyPair = () => crypto.generateKeyPairSync('rsa', {
+  modulusLength: 2048,
+  publicKeyEncoding: {
+    type: 'spki',
+    format: 'pem'
+  },
+  privateKeyEncoding: {
+    type: 'pkcs8',
+    format: 'pem',
+    cipher: 'aes-256-cbc',
+    passphrase: ''
+  }
+});
+
+const encrypt = (data, publicKey) => {
+  const encrypted = crypto.publicEncrypt(publicKey, Buffer.from(data));
+  return encrypted.toString('base64');
+};
+
+const decrypt = (ciphertext, privateKey) => {
+  const decrypted = crypto.privateDecrypt(
+    {
+      key: privateKey,
+      passphrase: '',
+    },
+    Buffer.from(ciphertext, 'base64')
+  );
+  return decrypted.toString('utf8');
+}
 
 (async () => {
-  try {
-    const firstNumber = await generatePrimeNumber(64);
-    const secondNumber = await generatePrimeNumber(64);
-
-    log(`First prime number: ${firstNumber}`);
-    log(`Second prime number: ${secondNumber}`);
-    log('Operations examples:');
-    operations.sum(firstNumber, secondNumber);
-    operations.pow2(firstNumber);
-
-    operations.mod(secondNumber, 27);
-    operations.mult(firstNumber, 9);
-  } catch (err) {
-    logError(err);
-  }
+  const data = 'Checking';
+  const keyPair = generateKeyPair();
+  const encrypted = encrypt(data, keyPair.publicKey);
+  const decrypted = decrypt(encrypted, keyPair.privateKey);
+  console.log('Data: ', data);
+  console.log('Code: ', encrypted);
+  console.log('Decode: ', decrypted);
 })();
+
+
+
